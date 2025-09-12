7faefde669eb040c454ca24b1175e749de96c6eb
Okoeguale Godwin <okoegualegodwin@gmail.com>
2025-09-12 15:11:25 +0100

    2-arguments.js file added

---
+++ 2-arguments.js
@@ -0,0 +1,11 @@
+const { argv } = require("node:process");
+
+const len = argv.length;
+
+if (len < 3) {
+  console.log("No argument");
+} else if (len === 3) {
+  console.log("Argument found");
+} else {
+  console.log("Arguments found");
+}
