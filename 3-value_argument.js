e3992693179b053289bd84277b4f65b9a24afef2
Okoeguale Godwin <okoegualegodwin@gmail.com>
2025-09-12 18:32:43 +0100

    edit file 3

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -6,7 +6,5 @@ for (length; argv[length]; length++);
 if (length <= 2) {
   console.log("No argument");
 } else {
-  const newArgs = argv.slice(2).join(" ");
-
-  console.log(newArgs);
+  console.log(argv[2]);
 }
