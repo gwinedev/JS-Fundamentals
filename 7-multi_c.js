a9211a4bba94e01878dd59d631d5bf43b523fc79
Okoeguale Godwin <okoegualegodwin@gmail.com>
2025-09-13 12:32:36 +0100

    add a check for negative argument

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,10 +1,13 @@
 const { argv } = require("node:process");
 
-let num = argv[2];
+let num = parseInt(argv[2]);
 
-if (!parseInt(argv[2])) {
+if (!num) {
   console.log("Missing number of occurrences");
 } else {
+  if (num < 1) {
+    return;
+  }
   while (num) {
     console.log("C is fun");
     num--;
